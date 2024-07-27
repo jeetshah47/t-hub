60d6d9f9d1def49cbd486013a2e60cef29567fb3
Jeet <jeet.shah@vayana.com>
2024-07-27 20:53:08 +0530

    pull chataf

--- src/module/utils/Firebase.ts
+++ src/module/utils/Firebase.ts
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAbPxBNmgQ4NfthpjIyqAHAtmI1aETp-IU",
   authDomain: "doorhub-2bd7c.firebaseapp.com",
@@ -13,8 +8,18 @@ const firebaseConfig = {
   storageBucket: "doorhub-2bd7c.appspot.com",
   messagingSenderId: "566142120152",
   appId: "1:566142120152:web:f524add0cc9237ea8667ad",
-  measurementId: "G-X9TVGB4R1H"
+  measurementId: "G-X9TVGB4R1H",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const uploadFiles = (file: File) => {
+  const storage = getStorage();
+  const sRef = ref(storage, "products/");
+
+  uploadBytes(sRef, file).then((snapshot) => {
+    console.log("Upload Successfull", snapshot);
+  });
+};
+
+export { app };
